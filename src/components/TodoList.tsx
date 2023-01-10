539cb40f90dfc128e3fc008e06ea2c0346765cf8
Vitor Hugo Pinto <vhpinto1@gmail.com>
2023-01-10 15:05:13 -0300

    Comportamento para lista vazia ou não

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -2,18 +2,40 @@ import { ClipboardText } from 'phosphor-react'
 
 import styles from './TodoList.module.css'
 
+interface Todo {
+    description: string;
+}
+
+const todoList: Todo[] = [{description: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.'}];
+
 export function TodoList () {
+    const isTodoListEmpty = todoList.length === 0 ? true : false;
+
     return (
         <div className={styles.todoList}>
             <header>
                 <p className={styles.tarefasCriadas}>Tarefas criadas <span> 0 </span></p>
                 <p className={styles.tarefasConcluidas}>Concluídas <span> 0 </span></p>
             </header>
-            <div>
-                <ClipboardText size={24} />
-                <strong>Você ainda não tem tarefas cadastradas</strong>
-                <p>Crie tarefas e organize seus itens a fazer</p>
-            </div>
+                { 
+                    isTodoListEmpty && 
+                
+                    <div className={styles.emptyList}>
+                        <ClipboardText size={56} />
+                        <strong>Você ainda não tem tarefas cadastradas</strong>
+                        <p>Crie tarefas e organize seus itens a fazer</p>
+                    </div>
+                }
+                { 
+                    !isTodoListEmpty && 
+                
+                    todoList.map((todo) => {
+                        return (
+                            <p>{todo.description}</p>
+                        )
+
+                    })
+                }
         </div>
     )
 }
