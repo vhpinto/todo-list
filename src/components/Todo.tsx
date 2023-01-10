0a76038a66f523f264a7625334f1e4baf5bf3332
Vitor Hugo Pinto <vhpinto1@gmail.com>
2023-01-10 17:12:11 -0300

    Estilização quando tarefa for concluida

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -5,8 +5,8 @@ import styles from './Todo.module.css'
 export function Todo () {
     return (
         <div className={styles.todo}>
-            <a href='#' className={styles.checked}><Circle size={24}/> </a>
-            <a href='#' className={styles.checked}><CheckCircle size={24} /> </a>
+            <a href='#' className={styles.notChecked}><Circle size={24}/> </a>
+            {/* <a href='#' className={styles.checked}><CheckCircle size={24} weight='fill' /> </a> */}
             
             
             <span>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</span>
