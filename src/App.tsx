3ce55cb98d636011a2682740f7aa0bb20411ecfe
Vitor Hugo Pinto <vhpinto1@gmail.com>
2023-01-10 10:53:48 -0300

    Componente AddTodoInput

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import { Header } from './components/Header'
 import styles from './App.module.css'
 
 import './global.css'
+import { AddTodoInput } from './components/AddTodoInput'
 
 export function App() {
   return (
     <div>
       <Header />
       <div className={styles.wrapper}>
-        <main></main>
+        <main>
+          <AddTodoInput />
+        </main>
       </div>
     </div>
   )
