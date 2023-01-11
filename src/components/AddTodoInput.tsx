82770dc073676ece858027c498ca588f15c96ddc
Vitor Hugo Pinto <vhpinto1@gmail.com>
2023-01-11 00:21:22 -0300

    Deixando botão de input funcional

--- src/components/AddTodoInput.tsx
+++ src/components/AddTodoInput.tsx
@@ -1,20 +1,44 @@
 import { PlusCircle } from "phosphor-react"
+import { ChangeEvent, FormEvent, useState } from "react";
+
+interface AddTodoProps {
+    onAddTodo: (description: string) => void;
+}
 
 import styles from './AddTodoInput.module.css'
 
-export function AddTodoInput() {
-    return (
-        <div className={styles.addTodo}>
-            <input 
-                type='text'
-                placeholder='Adicione uma nova tarefa'
-            >
+export function AddTodoInput({ onAddTodo }:AddTodoProps) {
+    const [ todoDescriptionValue, setTodoDescriptionValue ] = useState('');
 
-            </input>
-            <button>
-                <span>Criar</span> 
-                <PlusCircle size={24} />
-            </button>
+    function handleDescriptionValueChange(event: ChangeEvent<HTMLInputElement>) {
+        setTodoDescriptionValue(event.target.value)
+    }
+
+    function handlesAddTask(event: FormEvent){
+        event.preventDefault();
+
+        onAddTodo(todoDescriptionValue);
+        setTodoDescriptionValue('');
+    }
+
+    return (
+        <div >
+            <form onSubmit={handlesAddTask} className={styles.addTodo}>
+                <input 
+                    name='description'
+                    type='text'
+                    placeholder='Adicione uma nova tarefa'
+                    value={todoDescriptionValue}
+                    onChange={handleDescriptionValueChange}
+                    required
+                >
+
+                </input>
+                <button type='submit'>
+                    <span>Criar</span> 
+                    <PlusCircle size={24} />
+                </button>
+            </form>
         </div>
     )
 }
